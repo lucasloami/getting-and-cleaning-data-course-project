0950a1a09a5d4ac797cb697fd0240af03c358320
Lucas Lo Ami <loamilucas@gmail.com>
2015-05-24 17:11:33 -0300

    Filter mean and std dev variables. Add header to dataset

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,20 @@
 merge_datasets <- function() {
   training_set <- read.table("./data/train/X_train.txt")
   test_set <- read.table("./data/test/X_test.txt")
-  head(training_set)
-  summary(test_set)
+  header <- read.table("./data/features.txt")
+  data_set <- rbind(training_set, test_set)
+  colnames(data_set) <- header[,"V2"]
+  return(data_set)
+}
+
+filter_dataset <- function(rawdata) {
+  rd <- rawdata[grep("((mean|std){1}(freq){0,}[(][)])",names(rawdata))]
+  return(rd)
+}
+
+run_analysis <- function() {
+  dataset <- merge_datasets()
+  filtered_dataset <- filter_dataset(dataset)
 }
 
 download_data <- function() {
